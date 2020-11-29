bddb19c9c20afcd17d3ddbc722cbd7024d47530e
Paulo-PHP <phenrique.santana@hotmail.com>
2020-11-28 21:06:44 -0300

    implement method update in DepartmentDaoJDBC

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -1,5 +1,7 @@
 package application;
 
+import java.util.Scanner;
+
 import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
 import model.entities.Department;
@@ -8,12 +10,21 @@ public class Program2 {
 
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
+		
 		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 		
 		System.out.println("==== TEST 1: department insert ====");
 		Department department = new Department(null, "Games");
 		departmentDao.insert(department);
 		System.out.println("Inserted! New Id = " + department.getId());
+		
+		System.out.println("\n==== TEST 2: department update ====");
+		department = new Department(6, "Movies");
+		departmentDao.update(department);
+		System.out.println("Update complete!");
+		
+		sc.close();
 	}
 
 }
--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -3,10 +3,12 @@ package model.dao.impl;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.List;
 
 import db.DB;
+import db.DbException;
 import model.dao.DepartmentDao;
 import model.entities.Department;
 
@@ -35,16 +37,36 @@ public class DepartmentDaoJDBC implements DepartmentDao{
 				}
 				DB.closeResultSet(rs);
 			}
+			else {
+				throw new DbException("Unexpected error! No rows affected!");
+			}
 			
-		} catch (Exception e) {
-			// TODO: handle exception
+		} 
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
 		}
 	}
 
 	@Override
 	public void update(Department obj) {
-		// TODO Auto-generated method stub
-		
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement("UPDATE department SET Name = ? WHERE Id = ?");
+			
+			st.setString(1, obj.getName());
+			st.setInt(2, obj.getId());
+			
+			st.executeUpdate();
+		} 
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} 
+		finally {
+			DB.closeStatement(st);
+		}
 	}
 
 	@Override
